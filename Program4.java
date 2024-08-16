92d556f961751c20572f3a9a714e7afcc51de232
gamer6700k <62044986+gamer6700k@users.noreply.github.com>
2024-08-16 14:22:46 -0700

    Update Program4.java

--- Program4.java
+++ Program4.java
@@ -1,7 +1,32 @@
 //Youre on your own!
 //Jk ask for help if you need it (but also look at previous programs!!)
 
+import java.util.Scanner;
 
+public class Program4 {
+    public static void main(String[] args) { 
+    int variable1 = 0; 
+    int variable2 = 0; 
+    int variable3 = 0;
+    int variable4 = 0; 
+    int sum = 0; 
+    Scanner numScanner = new Scanner(System.in);
+    System.out.println("Enter the first number");
+    variable1 = numScanner.nextInt();
+    System.out.println("Enter the second number"); 
+    variable2 = numScanner.nextInt(); 
+    System.out.println("Enter the third number")
+    
+    
+    
+    }
+
+
+
+
+
+
+}
 
 //Paste console output below:
 /*
