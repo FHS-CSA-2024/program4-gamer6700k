2062131ae09dd1ef68bb00eece098b1caaeeb8b9
gamer6700k <62044986+gamer6700k@users.noreply.github.com>
2024-08-20 01:09:32 +0000

    finished

--- Program4.java
+++ Program4.java
@@ -10,14 +10,20 @@ public class Program4 {
     int variable3 = 0;
     int variable4 = 0; 
     int sum = 0; 
+    double average = 0;
     Scanner numScanner = new Scanner(System.in);
     System.out.println("Enter the first number");
     variable1 = numScanner.nextInt();
     System.out.println("Enter the second number"); 
     variable2 = numScanner.nextInt(); 
-    System.out.println("Enter the third number")
-    
-    
+    System.out.println("Enter the third number");
+    variable3 = numScanner.nextInt();
+    System.out.println("Enter the fourth number");
+    variable4 = numScanner.nextInt();
+    sum = variable1 + variable2 + variable3 + variable4;
+    System.out.println("The sum of the four numbers is: " + sum);
+    average=(variable1 + variable2 + variable3 + variable4)/4.0;
+    System.out.println("The average of the four numbers is: " + average);
     
     }
 
